d998168f7327495bed900ccd388fff6e83db5b98
sabbirziauddin <sabbir.ziauddin@gmail.com>
2023-03-23 16:19:11 +0100

    login page auth functional

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useState, useSyncExternalStore } from "react";
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../contexts/UseContext";
 import "./Login.css";
 
 const Login = () => {
+  const { logIn } = useContext(AuthContext);
+  const [error, setError] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    logIn(email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log("login page : ", user);
+        setError("");
+        form.reset();
+      })
+      .catch((err) => {
+        console.error(err);
+        setError(err.message);
+      });
+  };
   return (
     <div className="form-container ">
       <h2 className="form-title">Login</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="form-control">
           <label htmlFor="email">Email</label>
           <input type="email" name="email" id="" />
@@ -19,6 +40,7 @@ const Login = () => {
         <p>
           New to here ? <Link to="/signup"> Create New account</Link>
         </p>
+        {error && <p className="error-text">{error}</p>}
       </form>
     </div>
   );
--- src/contexts/UseContext.js
+++ src/contexts/UseContext.js
@@ -1,5 +1,9 @@
 import React, { createContext, useState } from "react";
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import app from "../firebase/firebase.init";
 
 export const AuthContext = createContext();
@@ -7,12 +11,16 @@ const auth = getAuth(app);
 
 const UseContext = ({ children }) => {
   const [name, setName] = useState({});
-
+  //create user with email and password
   const createUser = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
+  //create sign in
+  const logIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
   const user = { name: "anas" };
-  const authInfo = { user, createUser };
+  const authInfo = { user, createUser, logIn };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
